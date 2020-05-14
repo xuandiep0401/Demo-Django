1a72e1f24d5b179bb8cdb1af9894fc1bde9578e9
thanhnb <thanh.nguyenba0611@gmail.com>
2020-05-14 17:25:45 +0700

    update

--- app/dashboards/models.py
+++ app/dashboards/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    name = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
--- app/dashboards/tests.py
+++ app/dashboards/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from dashboards.models import Blog
 
 # Create your tests here.
+
+class DemoTestCase(TestCase):
+    def setUp(self):
+        Blog.objects.create(name="Demo 123", title="This is demo 123")
+        Blog.objects.create(name="Demo 321", title="This is demo 321")
+
+    def test_case_1(self):
+        blog_1 = Blog.objects.get(name="Demo 123")
+        blog_2 = Blog.objects.get(name="Demo 321")
+        self.assertEqual(blog_1.title, "This is demo 123")
+        self.assertEqual(blog_2.title, "This is demo 321")
+
+    def test_case_2(self):
+        blog_1 = Blog.objects.get(name="Demo 123")
+        blog_2 = Blog.objects.get(name="Demo 321")
+        self.assertEqual(blog_1.title, "This is demo 123")
+        self.assertEqual(blog_2.title, "This is demo 321")
+
+    def test_case_3(self):
+        blog_1 = Blog.objects.get(name="Demo 123")
+        blog_2 = Blog.objects.get(name="Demo 321")
+        self.assertEqual(blog_1.title, "This is demo 123")
+        self.assertEqual(blog_2.title, "This is demo 321")
+
+    def test_case_4(self):
+        blog_1 = Blog.objects.get(name="Demo 123")
+        blog_2 = Blog.objects.get(name="Demo 321")
+        self.assertEqual(blog_1.title, "This is demo 123")
+        self.assertEqual(blog_2.title, "This is demo 321")
